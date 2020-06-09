9c65146b75be2c04f71f0b40542423a69b3b64d4
Marc Wickenden <marc@4armed.com>
2020-06-09 15:43:01 +0100

    Working initial version from EC2 metadata

--- metacreds.go
+++ metacreds.go
@@ -3,19 +3,22 @@ package main
 import (
 	"fmt"
 
-	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/credentials/ec2rolecreds"
+	"github.com/aws/aws-sdk-go/aws/ec2metadata"
+	"github.com/aws/aws-sdk-go/aws/session"
 )
 
 func main() {
-	creds := credentials.NewCredentials(&ec2rolecreds.EC2RoleProvider{})
-
-	// Retrieve the credentials value
-	credValue, err := creds.Get()
-	if err != nil {
-		fmt.Printf(`Could not retrieve creds: %v`, err)
-		return
+	p := &ec2rolecreds.EC2RoleProvider{
+		Client: ec2metadata.New(session.New()),
 	}
 
-	fmt.Printf(`credValue: %v`, credValue)
+	creds, err := p.Retrieve()
+	if err != nil {
+		fmt.Printf("Retrieve creds error: %v", err)
+	}
+
+	fmt.Printf("export AWS_ACCESS_KEY_ID=%v", creds.AccessKeyID)
+	fmt.Printf("export AWS_SECRET_ACCESS_KEY=%v", creds.SecretAccessKey)
+	fmt.Printf("export AWS_SESSION_TOKEN=%v", creds.SessionToken)
 }
