4ea224c49cdf9054541d3ede559b6f6a7ac5de43
Marc Wickenden <marc@4armed.com>
2022-12-01 19:04:18 +0000

    Allow use of stdin with -s flag

--- main.go
+++ main.go
@@ -1,21 +1,47 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
 	"fmt"
+	"log"
+	"os"
 
 	metacredsaws "github.com/4armed/metacreds/pkg/aws"
 )
 
 func main() {
 
-	c, p := metacredsaws.New()
-	creds, err := metacredsaws.Retrieve(c, p)
-	if err != nil {
-		fmt.Printf("Retrieve creds error: %v", err)
+	var creds metacredsaws.Creds
+
+	stdin := flag.Bool("s", false, "process STDIN instead of metadata service")
+	region := flag.String("region", "eu-west-1", "AWS region")
+
+	flag.Parse()
+
+	if *stdin {
+		err := json.NewDecoder(os.Stdin).Decode(&creds)
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		c, p := metacredsaws.New()
+		metadataCreds, err := metacredsaws.Retrieve(c, p)
+		if err != nil {
+			fmt.Printf("Retrieve creds error: %v", err)
+			return
+		}
+
+		awsRegion, _ := c.Region()
+		region = &awsRegion
+
+		creds = metacredsaws.Creds{
+			AccessKeyID:     metadataCreds.AccessKeyID,
+			SecretAccessKey: metadataCreds.SecretAccessKey,
+			SessionToken:    metadataCreds.SessionToken,
+		}
 	}
 
-	region, _ := c.Region()
-
-	profile := metacredsaws.GenerateProfile("pentest", region, creds)
+	profile := metacredsaws.GenerateProfile("pentest", *region, creds)
 	fmt.Printf("\n%v\n", profile)
 }
--- pkg/aws/profile.go
+++ pkg/aws/profile.go
@@ -2,12 +2,21 @@ package aws
 
 import (
 	"fmt"
-
-	"github.com/aws/aws-sdk-go/aws/credentials"
 )
 
+type Creds struct {
+	// AWS Access key ID
+	AccessKeyID string `json:"AccessKeyID"`
+
+	// AWS Secret Access Key
+	SecretAccessKey string `json:"SecretAccessKey"`
+
+	// AWS Session Token
+	SessionToken string `json:"Token"`
+}
+
 // GenerateProfile creates an AWS CLI profile based on the supplied credentials
-func GenerateProfile(profileName, region string, creds credentials.Value) string {
+func GenerateProfile(profileName string, region string, creds Creds) string {
 	profile := `[profile %v]
 region = %v
 aws_access_key_id = %v
